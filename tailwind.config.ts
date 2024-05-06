90813b4e79d3ac8a720f2421bfea6e34b0cce33f
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-05-06 16:04:46 -0300

    feat: add eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "rgb(17 24 39)",
-        primary: "rgb(34 197 94)",
+        background: 'rgb(17 24 39)',
+        primary: 'rgb(34 197 94)',
       },
       maxWidth: {
-        default: "900px"
-      }
+        default: '900px',
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
